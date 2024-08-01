c57d7c8a9ca159ccfd29a028a8937653a7737390
sherlons <lukasteoo03@gmail.com>
2024-07-31 20:37:48 -0500

    figma

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let 
+    const textoUsuario = document.querySelector(".texto__a_encriptar");
+    const mensaje = document.querySelector(".mensaje");
+    
+///La letra "e" es convertida para "enter"
+///La letra "i" es convertida para "imes"
+///La letra "a" es convertida para "ai"
+///La letra "o" es convertida para "ober"
+///La letra "u" es convertida para "ufat"
+
+    function encriptar(stringEncriptada){
+
+    }
