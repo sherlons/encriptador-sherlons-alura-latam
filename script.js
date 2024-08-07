0c1fe8af02aa01436acd6411fe08b2410bbd8cd9
sherlons <lukasteoo03@gmail.com>
2024-08-06 22:11:47 -0500

    actualizacion script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// app.js
 const botonEncriptar = document.querySelector('.encriptar');
 const botonDesencriptar = document.querySelector('.desencriptar');
 const botonCopiar = document.querySelector('.btn_copia');
@@ -12,35 +13,31 @@ function btn_Encriptar() {
 }
 
 function encriptar(texto) {
-    let matrizCodigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     texto = texto.toLowerCase();
     for (let i = 0; i < matrizCodigo.length; i++) {
-        if (texto.includes(matrizCodigo[i][0])) {
-            texto = texto.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
-        }
+        texto = texto.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
     }
     return texto;
 }
 
 function btn_Desencriptar() {
-    const textoDesencriptado = desencriptar(textoAEncriptar.value);
+    const textoDesencriptado = desencriptar(mensaje.value);
     mensaje.value = textoDesencriptado;
     textoAEncriptar.value = ""; 
 }
 
-function desencriptar(texto) {
-    let matrizCodigo = [["ai", "a"], ["enter", "e"], ["imes", "i"], ["ober", "o"], ["ufat", "u"]];
-    texto = texto.toLowerCase();
+function desencriptar(textoD) {
+    let matrizCodigo = [["enter", "e"], ["imes", "i"], ["ai", "a"], ["ober", "o"], ["ufat", "u"]];
+    textoD = textoD.toLowerCase();
     for (let i = 0; i < matrizCodigo.length; i++) {
-        if (texto.includes(matrizCodigo[i][0])) {
-            texto = texto.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
-        }
+        textoD = textoD.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
     }
-    return texto;
+    return textoD;
 }
 
 function btn_copiar() {
     mensaje.select();
     document.execCommand("copy");
-    alert("Texto copiado al portapapeles");
+    alert("Texto copiado");
 }
